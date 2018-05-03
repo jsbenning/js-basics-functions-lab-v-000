63eb5092113060c3540af4419a1060e88e9ef43f
Josef Benninghoff <jsbenning4@gmail.com>
2018-05-03 23:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+  if (someValue >= 42) {
+    return someValue - 42;
+  } else {
+    return 42 - someValue;
+  }
+}
+ 
+function distanceFromHqInFeet (someValue) {
+  const blocks = distanceFromHqInBlocks(someValue);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(startTrip, endTrip) {
+  if (startTrip >= endTrip) {
+    const blocks = startTrip - endTrip;
+    return (blocks * 264);
+  } else {
+    const blocks = endTrip - startTrip;
+    return (blocks * 264);
+  }
+}
+
+
+function calculateFarePrice(startTrip, endTrip) {
+  const feet = (distanceTravelledInFeet(startTrip, endTrip) - 400);
+  if (feet <= 0) {
+    return 0;
+  } else if feet < 2000{
+    
+  } else {
+    return 'cannot travel that far';
+  }
+  
+}
+  
+}
